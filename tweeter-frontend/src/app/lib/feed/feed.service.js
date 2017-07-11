5111de18c529acab335d2fc00b93b0f89b982f22
Anthony Perry <atperry7@gmail.com>
2017-07-11 08:45:00 -0500

    Implemented logic to get a users specific tweets, but the server is doing a 302 redirect and shows it as failing, but the data is being returned correctly

--- tweeter-frontend/src/app/lib/feed/feed.service.js
+++ tweeter-frontend/src/app/lib/feed/feed.service.js
@@ -4,7 +4,7 @@
  * @Email:  atperry7@gmail.com
  * @Filename: feed.service.js
  * @Last modified by:   Anthony Perry
- * @Last modified time: 2017-07-10T19:34:09-05:00
+ * @Last modified time: 2017-07-11T08:39:49-05:00
  */
  export class FeedService {
    constructor ($q, localStorageService, $http, $log, $state) {
@@ -17,7 +17,24 @@
 
    displayTweets () {
      if (this.localStorageService.get('currentUser') !== null) {
-
+       let user = this.localStorageService.get('currentUser')
+       this.$log.log(`Check before server ${user.username}`)
+       this.$http({
+         method: 'GET',
+         url: 'http://localhost:8888/user/users/@' + user.username + '/feed'
+       }).then((response) => {
+         if (response.data !== undefined) {
+           for (let variable of response.data) {
+             this.$log.log(`User: ${variable.author.username}`)
+           }
+         }
+         this.$log.log(`Success going to server ${response.data}`)
+       }, (response) => {
+         this.$log.log(`Success going to and from server, but returned an error ${response.status} ${response.header} ${response.data}`)
+         for (let variable of response.data) {
+           this.$log.log(`User: ${variable.author.username}`)
+         }
+       })
      } else {
        this.$http({
          method: 'GET',
@@ -27,7 +44,6 @@
            for (let variable of response.data) {
              this.$log.log(`User: ${variable.author.username}`)
            }
-
          }
          this.$log.log(`Success going to and from server ${response.data}`)
        }, (response) => {
