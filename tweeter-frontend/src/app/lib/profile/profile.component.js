74842ad0926a3e09b354ba2372cdb0ad1b9afb89
LaVette Clifton <lavette.clifton@gmail.com>
2017-07-12 09:16:33 -0500

    profile: follow user, delete user, toggle views if current user

--- tweeter-frontend/src/app/lib/profile/profile.component.js
+++ tweeter-frontend/src/app/lib/profile/profile.component.js
@@ -21,13 +21,69 @@ const controller =
       this.localStorageService = localStorageService
     }
 
-    // getUserFollowing () {
-    //   return this.service.getUserFollowing(this.user.username)
-    // }
-    //
-    // getUserFollowers () {
-    //   return this.service.getUserFollowing(this.user.username)
-    // }
+    follow_btn_text = 'Follow'
+
+    get username () {
+      return this.user.username
+    }
+
+    get fName () {
+      return this.user.profile.firstName
+    }
+
+    get lName () {
+      return this.user.profile.lastName
+    }
+
+    get email () {
+      return this.user.profile.email
+    }
+
+    get phone () {
+      return this.user.profile.phone
+    }
+
+    get isCurrentUser () {
+      return this.service.isCurrentUser(this.username)
+    }
+
+    get isActive () {
+      return this.service.isActiveUser(this.username)
+    }
+
+    updateFollowBtn () {
+      this.follow_btn_text = this.service.updateFollowBtn(this.username)
+    }
+
+    followUser () {
+      if (this.service.isLoggedOn()) {
+        this.service.followUser(this.username)
+      } else {
+        this.service.errorMessage = 'You Must Log In To Follow This User: ' + this.username
+        this.$log.log('Not Logged In To Follow This User: ' + this.username)
+      }
+    }
+
+    unfollowUser () {
+      if (this.service.isLoggedOn()) {
+        this.service.unfollowUser(this.username)
+      } else {
+        this.service.errorMessage = 'Please Log In' + this.username
+        this.$log.log('Not Logged In To Unfollow This User: ' + this.username)
+      }
+    }
+
+    userExists () {
+      return this.service.userExists()
+    }
+
+    deleteUser () {
+      this.service.deleteUser()
+    }
+
+      get errorMessage () {
+        return this.service.errorMessage()
+      }
 
   }
 
@@ -35,7 +91,9 @@ export const twtrProfile = {
   controller,
   templateUrl,
   controllerAs: 'profile',
-  bindings: { user: '=',
+  bindings: {
+    user: '=',
     following: '=',
-    followers: '=' }
+    followers: '='
+  }
 }
--- tweeter-frontend/src/app/lib/profile/profile.service.js
+++ tweeter-frontend/src/app/lib/profile/profile.service.js
@@ -17,6 +17,46 @@ export class ProfileService {
     this.$state = $state
   }
 
+  errorMessage = ''
+
+  // Checks if a user is currently logged in
+  isLoggedOn () {
+    return this.localStorageService.get('currentUser') !== null
+  }
+
+ // Checks if the active page belongs to the current logged in user
+  isCurrentUser (username) {
+    if (this.isLoggedOn()) {
+      return this.localStorageService.get('currentUser').username === username
+    }
+  }
+
+  // Provides the text of the follow / unfollow btn based on current user
+  updateFollowBtn (un) {
+    // this.$log.log('Logged On: ' + this.isLoggedOn())
+    // if (this.isLoggedOn()) {
+    //   let currentUsername = this.localStorageService.get('currentUser').username
+    //   this.$log.log('Current User: ' + currentUsername)
+    //   let usersFollowed = this.getUserFollowing(currentUsername)
+    //   this.$log.log('usersFollowed: ' + usersFollowed[0])
+    //   this.$log.log('usersFollowed Username: ' + usersFollowed[0].username)
+    //   for (let followed in usersFollowed) {
+    //     this.$log.log('User: ' + followed.username)
+    //     if (followed.username === un) {
+    //       return 'Unfollow'
+    //     }
+    //   }
+    // } else {
+    return 'Follow'
+    // }
+  }
+
+// error display
+ errorMessage () {
+   return this.errorMessage
+ }
+
+  // Get user by username
   getUser (username) {
     let method = 'GET'
     let apiUrl = 'http://localhost:8888/user/users/@' + username
@@ -35,6 +75,7 @@ export class ProfileService {
     })
   }
 
+  // Get the users the active page's owner follows
   getUserFollowing (username) {
     let method = 'GET'
     let apiUrl = 'http://localhost:8888/user/users/@' + username + '/following'
@@ -47,29 +88,146 @@ export class ProfileService {
         'content-type': 'application/json'
       }
     }).then((response) => {
-      this.$log.log(`Response Data: ${response.data}`)
       return response.data
     }, (response) => {
-      this.$log.log(`Users being followed not found ${response.status}`)
+      this.$log.log(`Followed users not found, ${response.status}`)
     })
   }
 
+    // Get the active page's owner user's followers
     getUserFollowers (username) {
-    let method = 'GET'
-    let apiUrl = 'http://localhost:8888/user/users/@' + username + '/followers'
+      let method = 'GET'
+      let apiUrl = 'http://localhost:8888/user/users/@' + username + '/followers'
 
-    return this.$http({
-      method: method,
-      url: apiUrl,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'content-type': 'application/json'
-      }
-    }).then((response) => {
-      return response.data
-    }, (response) => {
-      this.$log.log(`Users following not found ${response.status}`)
-    })
-  }
+      return this.$http({
+        method: method,
+        url: apiUrl,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'content-type': 'application/json'
+        }
+      }).then((response) => {
+        return response.data
+      }, (response) => {
+        this.$log.log(`Users following not found ${response.status}`)
+      })
+    }
+
+    // Follow a user
+    followUser (username) {
+      let currentUsername = this.localStorageService.get('currentUser').username
+      let curentUserPass = this.localStorageService.get('password')
+      let method = 'POST'
+      let apiUrl = 'http://localhost:8888/user/users/@' + username + '/follow'
+      let requestBody = { credentials: { username: currentUsername, password: curentUserPass } }
+
+      this.$http({
+        method: method,
+        url: apiUrl,
+        data: requestBody
+      }).then((response) => {
+        this.$log.log(`User followed: ` + username + `, Status: ${response.status}`)
+      }, (response) => {
+        this.$log.log(`Unable to follow user ` + username + `, Status: ${response.status}`)
+      })
+    }
+
+    // Unfollow a user
+    unfollowUser (username) {
+      let method = 'POST'
+      let apiUrl = 'http://localhost:8888/user/users/@' + username + '/unfollow'
+      let currentUsername = this.localStorageService.get('currentUser').username
+      let curentUserPass = this.localStorageService.get('password')
+      let requestBody = { credentials: { username: currentUsername, password: curentUserPass } }
+
+      this.$http({
+        method: method,
+        url: apiUrl,
+        data: requestBody
+      }).then((response) => {
+        this.$log.log(`User unfollowed: ` + username + `, Status: ${response.status}`)
+      }, (response) => {
+        this.$log.log(`Unable to unfollow user: ` + username + `, Status: ${response.status}`)
+      })
+    }
+
+    // Update / Patch a user
+    updateUser (profile) {
+      let currentUsername = this.localStorageService.get('currentUser').username
+      let curentUserPass = this.localStorageService.get('password')
+      let method = 'PATCH'
+      let apiUrl = 'http://localhost:8888/user/users/@' + currentUsername
+      let requestBody = { credentials: { username: currentUsername, password: curentUserPass }, profile }
+
+      this.$http({
+        method: method,
+        url: apiUrl,
+        data: requestBody
+      }).then((response) => {
+        this.$log.log(`User deleted: ` + currentUsername + `, Status: ${response.status}`)
+      }, (response) => {
+        this.$log.log(`Unable to delete user: ` + currentUsername + `, Status: ${response.status}`)
+      })
+    }
+
+    userExists () {
+      let currentUsername = this.localStorageService.get('currentUser').username
+      let method = 'GET'
+      let apiUrl = 'http://localhost:8888/user/validate/username/exists/@' + currentUsername
+
+      this.$http({
+        method: method,
+        url: apiUrl,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'content-type': 'application/json'
+        }
+      }).then((response) => {
+        this.$log.log(`User deleted: ` + currentUsername + `, Status: ${response.status}`)
+        return response.data
+      }, (response) => {
+        this.$log.log(`Unable to delete user: ` + currentUsername + `, Status: ${response.status}`)
+      })
+    }
+
+    // Delete a user
+    deleteUser () {
+      let currentUsername = this.localStorageService.get('currentUser').username
+      let curentUserPass = this.localStorageService.get('password')
+      let method = 'DELETE'
+      let apiUrl = 'http://localhost:8888/user/users/@' + currentUsername
+      let requestBody = { credentials: { username: currentUsername, password: curentUserPass } }
+
+      this.$http({
+        method: method,
+        url: apiUrl,
+        data: requestBody
+      }).then((response) => {
+        this.$log.log(`User deleted: ` + currentUsername + `, Status: ${response.status}`)
+      }, (response) => {
+        this.$log.log(`Unable to delete user: ` + currentUsername + `, Status: ${response.status}`)
+      })
+    }
+
+    // Reactivate a user
+    activateUser () {
+      let currentUsername = this.localStorageService.get('currentUser').username
+      let curentUserPass = this.localStorageService.get('password')
+      let currentEmail = this.localStorageService.get('currentUser').profile.email
+      let method = 'POST'
+      let apiUrl = 'http://localhost:8888/user/users'
+      let requestBody = { credentials: { username: currentUsername, password: curentUserPass },
+        profile: { email: currentEmail } }
+
+      this.$http({
+        method: method,
+        url: apiUrl,
+        data: requestBody
+      }).then((response) => {
+        this.$log.log(`User reactivated: ` + currentUsername + `, Status: ${response.status}`)
+      }, (response) => {
+        this.$log.log(`Unable to reactivate user: ` + currentUsername + `, Status: ${response.status}`)
+      })
+    }
 
 }
