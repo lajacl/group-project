78621b6ac43dcb380b2f68fd25befe83b08c1a23
Anthony Perry <atperry7@gmail.com>
2017-07-13 08:38:24 -0500

    Added ngAnimate to package to attempt simple animations, also added some text that displays if a person doesnt have any tweets yet

--- tweeter-frontend/src/app/app.module.js
+++ tweeter-frontend/src/app/app.module.js
@@ -4,11 +4,12 @@
  * @Email:  crschmit@gmail.com
  * @Filename: app.module.js
  * @Last modified by:   Anthony Perry
- * @Last modified time: 2017-07-12T19:21:31-05:00
+ * @Last modified time: 2017-07-13T08:19:53-05:00
  */
 
  import ngLocalStorage from 'angular-local-storage'
  import ngUiRouter from 'angular-ui-router'
+ import ngAnimate from 'angular-animate'
 
  import twtrLogin from 'login/login.module'
  import twtrRegister from 'register/register.module'
@@ -29,6 +30,7 @@
   .module('twtr.app', [
     ngLocalStorage,
     ngUiRouter,
+    ngAnimate,
     twtrLogin,
     twtrRegister,
     twtrUsers,
