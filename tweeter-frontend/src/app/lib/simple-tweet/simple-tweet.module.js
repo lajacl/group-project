f806d6cf5c7e376e56d3b26ec5c5dc393f28e7cd
unknown <crschmit@gmail.com>
2017-07-12 20:44:01 -0500

    added simple-tweet module, for presenting tweet contents and author.username. injected simple-tweet into tweets module

---
+++ tweeter-frontend/src/app/lib/simple-tweet/simple-tweet.module.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: Christian Schmitt
+ * @Date:   2017-07-12T20:08:03-05:00
+ * @Email:  crschmit@gmail.com
+ * @Filename: simple-tweet.module.js
+ * @Last modified by:   Christian Schmitt
+ * @Last modified time: 2017-07-12T20:19:30-05:00
+ */
+
+import { simpleTweet } from 'simple-tweet/simple-tweet.component'
+
+export default ng
+  .module('simple-tweet', [])
+  .component('simpleTweet', simpleTweet)
+  .name
