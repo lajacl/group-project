e5987dbe99aa1335e75fc3771b4d3200c6a6bf1f
unknown <crschmit@gmail.com>
2017-07-13 17:04:59 -0500

    toggled ui-router visualizer

--- tweeter-frontend/src/app/app.run.js
+++ tweeter-frontend/src/app/app.run.js
@@ -4,7 +4,7 @@
  * @Email:  crschmit@gmail.com
  * @Filename: app.run.js
  * @Last modified by:   Christian Schmitt
- * @Last modified time: 2017-07-12T05:34:01-05:00
+ * @Last modified time: 2017-07-13T17:00:20-05:00
  */
 
 import { Visualizer } from '@uirouter/visualizer'
