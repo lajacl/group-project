120d76a2b97057f47c926da9acf7f02de981151f
unknown <crschmit@gmail.com>
2017-07-12 21:44:09 -0500

    connected Users Module to backend. simple-users in Users component link to profiles.

--- tweeter-frontend/src/app/lib/users/users.module.js
+++ tweeter-frontend/src/app/lib/users/users.module.js
@@ -4,13 +4,13 @@
  * @Email:  crschmit@gmail.com
  * @Filename: usesrs.module.js
  * @Last modified by:   Christian Schmitt
- * @Last modified time: 2017-07-12T21:13:58-05:00
+ * @Last modified time: 2017-07-12T21:41:40-05:00
  */
 
 import { UsersService } from 'users/users.service'
 import { twtrUsers } from 'users/users.component'
 import { config } from 'users/users.config'
-import usersData from 'users/users.data'
+// import usersData from 'users/users.data'
 
 import simpleUser from 'simple-user/simple-user.module'
 
@@ -18,6 +18,6 @@ export default ng
   .module('twtr.users', [simpleUser])
   .service('usersService', UsersService)
   .component('twtrUsers', twtrUsers)
-  .constant('usersData', usersData)
+  // .constant('usersData', usersData)
   .config(config)
   .name
